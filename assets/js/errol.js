9dca729e021a9de107fff1a5a11d911ec33f3ffe
Le Van Truong <23130357@st.hcmuaf.edu.vn>
2025-10-30 21:37:22 +0700

    chi tiết sách-mô tả sách

--- assets/js/errol.js
+++ assets/js/errol.js
@@ -1,9 +1,10 @@
 // show password
 const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
 const toggle = document.getElementById("togglePassword");
 
 toggle.addEventListener("click", () => {
     const type = password.type === "password" ? "text" : "password";
     password.type = type;
-    toggle.classList.toggle("fa-eye-slash");
+    confirmPassword.type = type;
 });
