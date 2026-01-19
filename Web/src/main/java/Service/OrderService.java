ad13d3da148e5511d7fd469007e1a12431b348f4
2130064-TruongCaoDoan <23130064@st.hcmuaf.edu.vn>
2026-01-19 20:42:37 +0700

    chức năng thêm sản phẩm vào giỏ hàng ở trang sản phẩm yêu thích

--- Web/src/main/java/Service/OrderService.java
+++ Web/src/main/java/Service/OrderService.java
@@ -12,10 +12,16 @@ public class OrderService {
         this.shippingDao = new ShippingDao();
     }
 
-    public boolean addOrder(int userId, double totalAmount, String note, int dis, int ship, int address_id, String shipping_type, double shipping_cost, String delivered_date) {
+    public boolean addOrder(int userId, double totalAmount, String note, Integer dis, Integer ship, Integer address_id, String shipping_type, double shipping_cost, String delivered_date) {
         int order_id = orderDao.addOrder(userId, totalAmount, note, dis, ship);
         int ship_id = shippingDao.addShipping(order_id, address_id, shipping_type, shipping_cost, delivered_date);
         return order_id !=-1 && ship_id !=-1;
+    }
+
+    public static void main(String[] args) {
+        OrderService orderService = new OrderService();
+        System.out.println(orderService.addOrder(31,2000,"hehe",13,14,9, "nhanh",20000,"22/10/2024"));
+
     }
 
 }
