8993f809c6109d231da9c7942554025feb803827
2130064-TruongCaoDoan <23130064@st.hcmuaf.edu.vn>
2026-01-19 19:54:10 +0700

    kiểm tra voucher áp dụng trong shopping cart hay thanh toán có phù hợp khi thêm sản phẩm khác, khi thoát ra thanh toán và thêm sản phẩm khác

--- Web/src/main/java/controler/Cart/ShoppingCart.java
+++ Web/src/main/java/controler/Cart/ShoppingCart.java
@@ -17,44 +17,60 @@ import java.util.List;
 public class ShoppingCart extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         VoucherService voucherService = new VoucherService();
         HttpSession session = request.getSession();
-        Cart cart = (Cart) session.getAttribute("cart");
-        double cartTotal = cart == null ? 0 : cart.getTotalBill();
-        User user = (User) session.getAttribute("user");
-        int id = user==null?0:user.getId();
 
-        double total = cart == null ? 0 : cart.getTotalBill();
+        Cart cart = (Cart) session.getAttribute("cart");
+        User user = (User) session.getAttribute("user");
+
+        int userId = user == null ? 0 : user.getId();
+        double cartTotal = cart == null ? 0 : cart.getTotalBill();
+
 
         Voucher applied = (Voucher) session.getAttribute("appliedDiscountVoucher");
-        double finalTotal = total;
+
+        if (applied != null) {
+            boolean valid = voucherService.isVoucherValid( cart,applied);
+            if (!valid) {
+                session.removeAttribute("appliedDiscountVoucher");
+                applied = null;
+                Integer numApplyVoucher= (Integer) session.getAttribute("numApplyVoucher");
+                numApplyVoucher--;
+                session.setAttribute("numApplyVoucher", numApplyVoucher);
+            }
+        }
+
+
+        double finalTotal = cartTotal;
 
         if (applied != null && "discount".equals(applied.getType())) {
             double value = applied.getValuee();
 
             if (value > 0) {
                 if (value < 1) {
-                    double discountAmount = total * value;
-                    finalTotal -= discountAmount;
+                    finalTotal -= cartTotal * value;
                 } else {
                     finalTotal -= value;
                 }
             }
-            if (finalTotal < 0) {
-                finalTotal = 0;
-            }
+            if (finalTotal < 0) finalTotal = 0;
         }
 
         request.setAttribute("finalTotal", finalTotal);
 
 
-        List<Voucher> listVoucherDiscount =voucherService.listVoucherDiscountUser(id);
-        List<Voucher> listVoucherShip =voucherService.listVoucherShipUser(id);
+        List<Voucher> listVoucherDiscount = voucherService.listVoucherDiscountUser(userId);
+        List<Voucher> listVoucherShip = voucherService.listVoucherShipUser(userId);
+
         listVoucherDiscount = voucherService.filterVoucherValid(cart, cartTotal, listVoucherDiscount);
         listVoucherShip = voucherService.filterVoucherValid(cart, cartTotal, listVoucherShip);
-        request.setAttribute("listVoucherDiscount",listVoucherDiscount);
-        request.setAttribute("listVoucherShip",listVoucherShip);
-        request.getRequestDispatcher("user/shoppingCart.jsp").forward(request,response);
+
+        request.setAttribute("listVoucherDiscount", listVoucherDiscount);
+        request.setAttribute("listVoucherShip", listVoucherShip);
+
+        request.getRequestDispatcher("user/shoppingCart.jsp")
+                .forward(request, response);
     }
 
     @Override
