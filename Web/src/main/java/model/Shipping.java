5cb63a9a34927d6b48bcbe1dd44f75ebb4855157
ghuy285 <23130134@st.hcmuaf.edu.vn>
2026-01-26 18:25:11 +0700

    fill attr for Shipping

--- Web/src/main/java/model/Shipping.java
+++ Web/src/main/java/model/Shipping.java
@@ -1,4 +1,69 @@
 package model;
 
+import java.util.Date;
+
 public class Shipping {
+    private int orderId;
+    private int addressId;
+    private String shippingType;
+    private Double shippingCost;
+    private Date shippingDate;
+    private Date deliveredDate;
+    private String status;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getShippingType() {
+        return shippingType;
+    }
+
+    public void setShippingType(String shippingType) {
+        this.shippingType = shippingType;
+    }
+
+    public Double getShippingCost() {
+        return shippingCost;
+    }
+
+    public void setShippingCost(Double shippingCost) {
+        this.shippingCost = shippingCost;
+    }
+
+    public Date getShippingDate() {
+        return shippingDate;
+    }
+
+    public void setShippingDate(Date shippingDate) {
+        this.shippingDate = shippingDate;
+    }
+
+    public Date getDeliveredDate() {
+        return deliveredDate;
+    }
+
+    public void setDeliveredDate(Date deliveredDate) {
+        this.deliveredDate = deliveredDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
